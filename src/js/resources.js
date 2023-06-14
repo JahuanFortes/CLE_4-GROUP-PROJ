7fb6cfc400cb0df5482575b4d0dfaf0b06a66623
AnnyXi <1061555@hr.nl>
2023-06-14 12:59:12 +0200

    size scale for characters

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,14 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import fishImage from '../images/fish.png'
+import map from '../images/map.jpg'
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
+    Fish: new ImageSource(fishImage),
+    Map: new ImageSource(map)
 }
-const ResourceLoader = new Loader([Resources.Fish])
+const ResourceLoader = new Loader([
+    Resources.Fish,
+    Resources.Map
+])
 
 export { Resources, ResourceLoader }
