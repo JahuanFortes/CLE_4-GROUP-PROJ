23968896936d87465b0593009fb15fe13c1d8bfe
TEGJoy <terencio_12@live.nl>
2023-06-14 14:54:28 +0200

    We need to use branches

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,10 +1,16 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import fishImage from '../images/fish.png'
 import block from '../images/Sprite-0002-rock.png'
+import mapImage from "../images/map.jpg"
+import fontFile from "../css/PressStart2P-Regular.ttf";
+const font = new FontFace("PressStart2P-Regular", `url(${fontFile})`);
+document.fonts.add(font);
+font.load();
 const Resources = {
     Fish: new ImageSource(fishImage),
     block: new ImageSource(block),
+    Map: new ImageSource(mapImage)
 }
-const ResourceLoader = new Loader([Resources.Fish, Resources.block])
+const ResourceLoader = new Loader([Resources.Fish, Resources.block,Resources.Map])
 
 export { Resources, ResourceLoader };
