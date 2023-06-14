8bd8b0eb1bc652f9b0da56aa85e6ac227c2c52b9
AnnyXi <1061555@hr.nl>
2023-06-14 13:42:16 +0200

    add image to resource

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,5 +1,6 @@
 import { ImageSource, Sound, Resource, Loader } from "excalibur";
 import fishImage from "../images/fish.png";
+import mapImage from "../images/map.jpg"
 import fontFile from "../css/PressStart2P-Regular.ttf";
 const font = new FontFace("PressStart2P-Regular", `url(${fontFile})`);
 document.fonts.add(font);
@@ -7,7 +8,11 @@ font.load();
 
 const Resources = {
   Fish: new ImageSource(fishImage),
+  Map: new ImageSource(mapImage)
 };
-const ResourceLoader = new Loader([Resources.Fish]);
+const ResourceLoader = new Loader([
+    Resources.Fish,
+    Resources.Map
+]);
 
 export { Resources, ResourceLoader };
