c516015da4b6019334131fd80b3027c806a2c3fc
Jahuan Fortes <marleyjahuan@gmail.com>
2023-06-16 13:00:29 +0200

    test

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,26 +1,27 @@
-import { ImageSource, Sound, Resource, Loader } from 'excalibur'
-import fishImage from '../images/fish.png'
-import block from '../images/Sprite-0002-rock.png'
-import mapImage from "../images/map.jpg"
-import level1 from "../images/BgLevel1.jpg"
-import testButton from "../images/test-button.png"
-import testButton2 from "../images/test-button-2.png"
+import { ImageSource, Sound, Resource, Loader } from "excalibur";
+import fishImage from "../images/fish.png";
+import block from "../images/Sprite-0002-rock.png";
+import mapImage from "../images/map.jpg";
+import level1 from "../images/BgLevel1.jpg";
+import testButton from "../images/test-button.png";
+import testButton2 from "../images/test-button-2.png";
 import fontFile from "../css/PressStart2P-Regular.ttf";
+
 const font = new FontFace("PressStart2P-Regular", `url(${fontFile})`);
 document.fonts.add(font);
 font.load();
 const Resources = {
-    Fish: new ImageSource(fishImage),
-    block: new ImageSource(block),
-    Map: new ImageSource(mapImage),
-    Level1: new ImageSource(level1),
-    TestButton: new ImageSource(testButton),
-    TestButton2: new ImageSource(testButton2)
-}
+  Fish: new ImageSource(fishImage),
+  block: new ImageSource(block),
+  Map: new ImageSource(mapImage),
+  Level1: new ImageSource(level1),
+  TestButton: new ImageSource(testButton),
+  TestButton2: new ImageSource(testButton2),
+};
 // met deze for loop hoef je niet alles handmatig in de loader te zetten
-const resourceArray = []
+const resourceArray = [];
 for (const key in Resources) {
-    resourceArray.push(Resources[key])
+  resourceArray.push(Resources[key]);
 }
-const ResourceLoader = new Loader(resourceArray)
-export { Resources, ResourceLoader }
+const ResourceLoader = new Loader(resourceArray);
+export { Resources, ResourceLoader };
