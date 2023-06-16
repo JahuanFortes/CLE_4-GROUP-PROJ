5676af9422929508edbd6424468217b1fd4696a6
TEGJoy <terencio_12@live.nl>
2023-06-16 13:42:48 +0200

    Created basic logic for charselect

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,14 +3,16 @@ import fishImage from '../images/fish.png'
 import block from '../images/Sprite-0002-rock.png'
 import mapImage from "../images/map.jpg"
 import fontFile from "../css/PressStart2P-Regular.ttf";
+import characterSheet from "../images/character1.png"
 const font = new FontFace("PressStart2P-Regular", `url(${fontFile})`);
 document.fonts.add(font);
 font.load();
 const Resources = {
     Fish: new ImageSource(fishImage),
     block: new ImageSource(block),
-    Map: new ImageSource(mapImage)
+    Map: new ImageSource(mapImage),
+    characterSheet: new ImageSource(characterSheet),
 }
-const ResourceLoader = new Loader([Resources.Fish, Resources.block,Resources.Map])
+const ResourceLoader = new Loader([Resources.Fish, Resources.block,Resources.Map, Resources.characterSheet])
 
 export { Resources, ResourceLoader };
