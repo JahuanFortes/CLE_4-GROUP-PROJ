019837f598de2558b10ed11ccb6547316de04857
Jahuan Fortes <marleyjahuan@gmail.com>
2023-06-22 13:26:40 +0200

    done with my part

--- src/js/scenes/game_lvl2.js
+++ src/js/scenes/game_lvl2.js
@@ -40,9 +40,6 @@ export class Level2 extends Scene {
     this.button2 = new IngameButton(1950, 650, 2, this);
     this.button3 = new IngameButton(100, 1150, 2, this);
     this.button4 = new IngameButton(100, 900, 3, this);
-
-    // this.mazeButton = new IngameButton(750, 800, 2, this);
-    // this.mazeButton2 = new IngameButton(250, 500, 1, this);
     //#endregion buttons
 
     //#region walls
@@ -50,38 +47,23 @@ export class Level2 extends Scene {
     this.wall2 = new Wall(-5, 310, 215, 310);
     this.wall3 = new Wall(1810, 754, 2100, 754);
     this.wall4 = new Wall(-672, 836, -672, 1180);
-
-    // this.breakingWall = new Wall(1710, 323, 845, 323);
-    // this.breakingWallImg = new MovableObject(1, Resources.Onewall.toSprite(), CollisionType.Passive, 400, 30);
-    this.breakingWall2 = new Wall(845, 323, 845, 0);
-    this.breakingWall3 = new Wall(300, 550, 300, 850);
     //#endregion walls
 
     //#region boxes
     this.boxes = new Box(1466, 270, 2500, 88);
-    // this.boxes2 = new Box(-500, 270, 1000, 88);
+    this.boxes2 = new Box(-500, 270, 1000, 88);
     this.boxes3 = new Box(-1030, -40, 88, 900);
     this.boxes4 = new Box(2600, -40, 88, 850);
-    this.boxes5 = new Box(1690, 600, 100, 600);
-
+    this.boxes5 = new Box(1650, 640, 50, 240);
+    this.boxes6 = new Box(2222, 640, 50, 240);
+    this.boxes7 = new Box(1890, 530, 610, 50);
+    this.boxes8 = new Box(2215, 740, 100, 40);
+    this.boxes9 = new Box(1700, 740, 100, 40);
+    this.boxes10 = new Box(-900, 740, 500, 100);
+    this.boxes11 = new Box(-900, 1240, 500, 100);
     //#endregion boxes
 
     //#region objects
-    this.movableObject = new MovableObject(
-      3,
-      Resources.Smallstone.toSprite(),
-      CollisionType.Active,
-      300,
-      100
-    );
-    this.moveStone = new MovableObject(
-      1,
-      Resources.Stone.toSprite(),
-      CollisionType.Active,
-      880,
-      510
-    );
-    //#endregion objects
     this.ending = new MovableObject(
       1,
       Resources.Ending.toSprite(),
@@ -89,10 +71,13 @@ export class Level2 extends Scene {
       -900,
       1000
     );
+    //#endregion objects
   }
 
   onInitialize(engine) {
     //gets engine to use for controllers, also adds background with right scale and pos
+
+    //#region background
     this.game = engine;
     engine.input.gamepads.enabled = true;
     let background = new Actor();
@@ -100,109 +85,98 @@ export class Level2 extends Scene {
     background.scale = new Vector(2.6, 2.2);
     background.pos = new Vector(775, 480);
     this.add(background);
+    //#endregion background
   }
 
   onActivate(ctx) {
     //adds players to scene and gives them the right id and location
     console.log("Level1 has started");
     console.log(ctx.previousScene);
+
+    //#region Players
     this.player1ID = ctx.previousScene.player1ID;
     this.player2ID = ctx.previousScene.player2ID;
-    this.player = new player(this, 1, 40, -100, this.player1ID);
-    this.player2 = new player(this, 2, 5, 100, this.player2ID);
+    this.player = new player(this, 1, 2400, 1000, this.player1ID);
+    this.player2 = new player(this, 2, 2400, -100, this.player2ID);
+    //#endregion Players
+
     //starts game
     this.startGame();
   }
 
   startGame() {
     this.game.currentScene.camera.strategy.lockToActor(this.player);
+
     this.add(this.player);
-    console.log(this.player);
-    //#region addedButtons
+
+    //#region Buttons
     this.add(this.button1);
     this.add(this.button2);
     this.add(this.button3);
     this.add(this.button4);
+    //#endregion Buttons
 
-    // this.add(this.mazeButton);
-    // this.add(this.mazeButton2);
-    //#endregion addedButtons
-
-    //#region addedWalls
-    // this.add(this.breakingWall);
-    // this.add(this.breakingWall2);
-    // this.add(this.breakingWall3);
-    // this.add(this.wall);
-    // this.add(this.wall2);
+    //#region Walls
     this.add(this.wall);
     this.add(this.wall2);
     this.add(this.wall3);
     this.add(this.wall4);
+    //#endregion Walls
 
-    //#endregion addedWalls
-
-    //#region addedBoxes
+    //#region Boxes
     //all boxes used for the maze
     this.add(this.boxes);
-    // this.add(this.boxes2);
+    this.add(this.boxes2);
     this.add(this.boxes3);
     this.add(this.boxes4);
     this.add(this.boxes5);
+    this.add(this.boxes6);
+    this.add(this.boxes7);
+    this.add(this.boxes8);
+    this.add(this.boxes9);
+    this.add(this.boxes10);
+    this.add(this.boxes11);
+    //#endregion Boxes
 
-    //#endregion addedBoxes
-
-    // this.add(this.movableObject);
-    // this.add(this.moveStone);
     this.add(this.ending);
   }
-  /*
-    loadEverything() {
-      this.add(this.player)
 
-    }
-    */
   onPreUpdate(game) {
     //this is puzzle one, both press the button and the door opens
     //these are all if statements used to check for buttons etc.
-    // if (this.button1.openDoor && this.button2.openDoor) {
-    //   Resources.Opendoor.play();
-    //   this.breakingWall.kill();
-    //   this.button1.openDoor = false;
-    //   this.button2.openDoor = false;
-    // }
+    //#region Puzzles
+    if (this.button3.openDoor && this.button4.openDoor) {
+      Resources.Opendoor.play();
+      this.wall4.kill();
+      this.button3.openDoor = false;
+      this.button4.openDoor = false;
+    }
 
-    // if (this.mazeButton.openDoor && this.mazeButton2.openDoor) {
-    //   this.breakingWall3.collider.set(null);
-    //   this.mazeButton.openDoor = false;
-    //   this.mazeButton2.openDoor = false;
-    // }
+    if (this.button1.openDoor) {
+      this.wall3.kill();
+      this.button1.openDoor = false;
+    }
 
-    //this is puzzle two, if the button gets pressed
-    // if (this.button3.openDoor) {
-    //   this.breakingWall2.collider.set(null);
-    //   this.button3.opendoor = false;
-    // } else {
-    //   this.breakingWall2.collider.set(this.breakingWall2.waall);
-    //   this.button3.opendoor = false;
-    // }
-
-    //last button checker, only works if movable object is used
-    // if (this.button4.openDoor) {
-    //   //   Resources.Opendoor.play();
-    //   this.breakingWall2.kill();
-    //   this.button4.opendoor = false;
-    // }
+    if (this.button2.openDoor) {
+      Resources.Opendoor.play();
+      this.wall.kill();
+      this.wall2.kill();
+      this.button2.openDoor = false;
+    }
 
     if (this.ending.isColliding) {
       this.game.startLevel2();
       this.ending.isColliding = false;
     }
+    //#endregion Puzzles
 
+    //#region Controller
     //also adds keyboard and controller to use ingame
     let kb = this.game.input.keyboard;
     let controller = this.game.input.gamepads;
     if (kb.isHeld(Input.Keys.Enter)) {
       this.add(this.player2);
     }
+    //#endregion Controller
   }
 }
