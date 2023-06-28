6f34475bf717324cccc142eaa691294c62c3640a
Jahuan Fortes <marleyjahuan@gmail.com>
2023-06-28 12:18:22 +0200

    finished product

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,115 +1,119 @@
-import {ImageSource, Sound, Resource, Loader, Color} from 'excalibur'
-import logo from "../images/logo.png"
-import start from "../images/startButton.png"
-import fishImage from '../images/fish.png'
-import signup from '../images/sign.png'
-import signdown from '../images/sign_down.png'
-import labellocation from '../images/pigeon.png'
-import block from '../images/Sprite-0002-rock.png'
-import mapImage from "../images/map.jpg"
-import background from "../images/bg.jpg"
-import level1 from "../images/BgLevel1.jpg"
+import { ImageSource, Sound, Resource, Loader, Color } from "excalibur";
+import logo from "../images/logo.png";
+import start from "../images/startButton.png";
+import fishImage from "../images/fish.png";
+import signup from "../images/sign.png";
+import signdown from "../images/sign_down.png";
+import labellocation from "../images/pigeon.png";
+import block from "../images/Sprite-0002-rock.png";
+import mapImage from "../images/map.jpg";
+import background from "../images/bg.jpg";
+import level1 from "../images/BgLevel1.jpg";
 import level2 from "../images/BgLevel2New.jpg";
-import realLevel from "../images/BgLevel1New.jpg"
-import realLevel3 from "../images/BgLevel3New.jpg"
-import backgroundstart from "../images/BgLevel4New.jpg"
-import bgtest from "../images/bgtest.jpg"
-import testButton from "../images/test-button.png"
-import testButton2 from "../images/test-button-2.png"
-import stone from "../images/stone.png"
-import moveWall from "../images/level1WallMove.png"
-import moveRock from "../images/level1rockMove.png"
-import tree from "../images/tree.png"
-import lvl2moveWall from "../images/level2Room1Move.png"
-import lvl2moveWalltwo from "../images/level2WallMove.png"
-import lvl2MoveRoom from "../images/level2Room2Move.png"
-import football from "../images/football.png"
-import tennisball from "../images/tennis-ball.png"
-import fence from "../images/fence.png"
-import smallstone from "../images/smallStone.png"
-import ending from "../images/endingLvl1.png"
+import realLevel from "../images/BgLevel1New.jpg";
+import realLevel3 from "../images/BgLevel3New.jpg";
+import backgroundstart from "../images/BgLevel4New.jpg";
+import bgtest from "../images/bgtest.jpg";
+import testButton from "../images/test-button.png";
+import testButton2 from "../images/test-button-2.png";
+import stone from "../images/stone.png";
+import moveWall from "../images/level1WallMove.png";
+import moveRock from "../images/level1rockMove.png";
+import tree from "../images/tree.png";
+import lvl2moveWall from "../images/level2Room1Move.png";
+import lvl2moveWalltwo from "../images/level2WallMove.png";
+import lvl2MoveRoom from "../images/level2Room2Move.png";
+import football from "../images/football.png";
+import tennisball from "../images/tennis-ball.png";
+import fence from "../images/fence.png";
+import smallstone from "../images/smallStone.png";
+import ending from "../images/endingLvl1.png";
 import fontFile from "../css/PressStart2P-Regular.ttf";
-import leaf from "../images/leaf.png"
-import click from "../sounds/click.wav"
-import offclick from "../sounds/offclick.wav"
-import opendoor from "../sounds/explosion.wav"
-import pickup from "../sounds/pickupLeaf.wav"
-import pigeonSound from "../sounds/pigeon.wav"
-import characterSheet from "../images/character1.png"
-import characterFWalkSheet from "../images/BASE_character F_walk sprite.png"
-import CharacterSWalkSheet from "../images/BASE_character S_walk sprite.png"
-import CharacterFWalkSheet1 from "../images/1_character F_walk sprite.png"
-import CharacterFWalkSheet2 from "../images/2_character F_walk sprite.png"
-import CharacterFWalkSheet3 from "../images/3_character F_walk sprite.png"
-import CharacterFWalkSheet4 from "../images/4_character F_walk sprite.png"
-import CharacterBWalkSheet1 from "../images/1_character B_walk sprite.png"
-import CharacterBWalkSheet2 from "../images/2_character B_walk sprite.png"
-import CharacterBWalkSheet3 from "../images/3_character B_walk sprite.png"
-import CharacterBWalkSheet4 from "../images/4_character B_walk sprite.png"
-import CharacterSWalkSheet1 from "../images/1_character S_walk sprite.png"
-import CharacterSWalkSheet2 from "../images/2_character S_walk sprite.png"
-import CharacterSWalkSheet3 from "../images/3_character S_walk sprite.png"
-import CharacterSWalkSheet4 from "../images/4_character S_walk sprite.png"
-import enemyOne from "../images/idle.png"
-import enemyTwo from "../images/idle2.png"
+import leaf from "../images/leaf.png";
+import click from "../sounds/click.wav";
+import offclick from "../sounds/offclick.wav";
+import opendoor from "../sounds/explosion.wav";
+import pickup from "../sounds/pickupLeaf.wav";
+import pigeonSound from "../sounds/pigeon.wav";
+import characterSheet from "../images/character1.png";
+import characterFWalkSheet from "../images/BASE_character F_walk sprite.png";
+import CharacterSWalkSheet from "../images/BASE_character S_walk sprite.png";
+import CharacterFWalkSheet1 from "../images/1_character F_walk sprite.png";
+import CharacterFWalkSheet2 from "../images/2_character F_walk sprite.png";
+import CharacterFWalkSheet3 from "../images/3_character F_walk sprite.png";
+import CharacterFWalkSheet4 from "../images/4_character F_walk sprite.png";
+import CharacterBWalkSheet1 from "../images/1_character B_walk sprite.png";
+import CharacterBWalkSheet2 from "../images/2_character B_walk sprite.png";
+import CharacterBWalkSheet3 from "../images/3_character B_walk sprite.png";
+import CharacterBWalkSheet4 from "../images/4_character B_walk sprite.png";
+import CharacterSWalkSheet1 from "../images/1_character S_walk sprite.png";
+import CharacterSWalkSheet2 from "../images/2_character S_walk sprite.png";
+import CharacterSWalkSheet3 from "../images/3_character S_walk sprite.png";
+import CharacterSWalkSheet4 from "../images/4_character S_walk sprite.png";
+import enemyOne from "../images/idle.png";
+import enemyTwo from "../images/idle2.png";
+// import bgMusic from "../sounds/BG_music.mp3";
+// import battle from "../sounds/The Last Encounter (90s RPG Version).mp3";
 
 const font = new FontFace("PressStart2P-Regular", `url(${fontFile})`);
 document.fonts.add(font);
 font.load();
 const Resources = {
-    Signup: new ImageSource(signup),
-    Signdown: new ImageSource(signdown),
-    Labellocation: new ImageSource(labellocation),
-    Fish: new ImageSource(fishImage),
-    block: new ImageSource(block),
-    Map: new ImageSource(mapImage),
-    Bg: new ImageSource(background),
-    Football: new ImageSource(football),
-    Tennisball: new ImageSource(tennisball),
-    Level1: new ImageSource(level1),
-    Bgtest: new ImageSource(bgtest),
-    realLevel: new ImageSource(realLevel),
-    realLevel3: new ImageSource(realLevel3),
-    Backbroundstart: new ImageSource(backgroundstart),
-    TestButton: new ImageSource(testButton),
-    TestButton2: new ImageSource(testButton2),
-    Stone: new ImageSource(stone),
-    Leaf: new ImageSource(leaf),
-    Onewall: new ImageSource(moveWall),
-    Moverock: new ImageSource(moveRock),
-    Level2Onewall: new ImageSource(lvl2moveWall),
-    Level2Movewall: new ImageSource(lvl2moveWalltwo),
-    Level2Twowall: new ImageSource(lvl2MoveRoom),
-    Tree: new ImageSource(tree),
-    Fence: new ImageSource(fence),
-    characterSheet: new ImageSource(characterSheet),
-    Smallstone: new ImageSource(smallstone),
-    Ending: new ImageSource(ending),
-    Level2: new ImageSource(level2),
-    Click: new Sound(click),
-    Offclick: new Sound(offclick),
-    Opendoor: new Sound(opendoor),
-    Pickupleaf: new Sound(pickup),
-    Pigeonsound: new Sound(pigeonSound),
-    CharacterSWalkSheet: new ImageSource(CharacterSWalkSheet),
-    CharacterFWalkSheet: new ImageSource(characterFWalkSheet),
-    Enemy1WalkSheet: new ImageSource(enemyOne),
-    Enemy2WalkSheet: new ImageSource(enemyTwo),
-    Logo: new ImageSource(logo),
-    Start: new ImageSource(start),
-    CharacterSWalkSheet1: new ImageSource(CharacterSWalkSheet1),
-    CharacterSWalkSheet2: new ImageSource(CharacterSWalkSheet2),
-    CharacterSWalkSheet3: new ImageSource(CharacterSWalkSheet3),
-    CharacterSWalkSheet4: new ImageSource(CharacterSWalkSheet4),
-    CharacterFWalkSheet1: new ImageSource(CharacterFWalkSheet1),
-    CharacterFWalkSheet2: new ImageSource(CharacterFWalkSheet2),
-    CharacterFWalkSheet3: new ImageSource(CharacterFWalkSheet3),
-    CharacterFWalkSheet4: new ImageSource(CharacterFWalkSheet4),
-    CharacterBWalkSheet1: new ImageSource(CharacterBWalkSheet1),
-    CharacterBWalkSheet2: new ImageSource(CharacterBWalkSheet2),
-    CharacterBWalkSheet3: new ImageSource(CharacterBWalkSheet3),
-    CharacterBWalkSheet4: new ImageSource(CharacterBWalkSheet4)
-}
+  Signup: new ImageSource(signup),
+  Signdown: new ImageSource(signdown),
+  Labellocation: new ImageSource(labellocation),
+  Fish: new ImageSource(fishImage),
+  block: new ImageSource(block),
+  Map: new ImageSource(mapImage),
+  Bg: new ImageSource(background),
+  Football: new ImageSource(football),
+  Tennisball: new ImageSource(tennisball),
+  Level1: new ImageSource(level1),
+  Bgtest: new ImageSource(bgtest),
+  realLevel: new ImageSource(realLevel),
+  realLevel3: new ImageSource(realLevel3),
+  Backbroundstart: new ImageSource(backgroundstart),
+  TestButton: new ImageSource(testButton),
+  TestButton2: new ImageSource(testButton2),
+  Stone: new ImageSource(stone),
+  Leaf: new ImageSource(leaf),
+  Onewall: new ImageSource(moveWall),
+  Moverock: new ImageSource(moveRock),
+  Level2Onewall: new ImageSource(lvl2moveWall),
+  Level2Movewall: new ImageSource(lvl2moveWalltwo),
+  Level2Twowall: new ImageSource(lvl2MoveRoom),
+  Tree: new ImageSource(tree),
+  Fence: new ImageSource(fence),
+  characterSheet: new ImageSource(characterSheet),
+  Smallstone: new ImageSource(smallstone),
+  Ending: new ImageSource(ending),
+  Level2: new ImageSource(level2),
+  Click: new Sound(click),
+  Offclick: new Sound(offclick),
+  Opendoor: new Sound(opendoor),
+  Pickupleaf: new Sound(pickup),
+  Pigeonsound: new Sound(pigeonSound),
+  CharacterSWalkSheet: new ImageSource(CharacterSWalkSheet),
+  CharacterFWalkSheet: new ImageSource(characterFWalkSheet),
+  Enemy1WalkSheet: new ImageSource(enemyOne),
+  Enemy2WalkSheet: new ImageSource(enemyTwo),
+  Logo: new ImageSource(logo),
+  Start: new ImageSource(start),
+  CharacterSWalkSheet1: new ImageSource(CharacterSWalkSheet1),
+  CharacterSWalkSheet2: new ImageSource(CharacterSWalkSheet2),
+  CharacterSWalkSheet3: new ImageSource(CharacterSWalkSheet3),
+  CharacterSWalkSheet4: new ImageSource(CharacterSWalkSheet4),
+  CharacterFWalkSheet1: new ImageSource(CharacterFWalkSheet1),
+  CharacterFWalkSheet2: new ImageSource(CharacterFWalkSheet2),
+  CharacterFWalkSheet3: new ImageSource(CharacterFWalkSheet3),
+  CharacterFWalkSheet4: new ImageSource(CharacterFWalkSheet4),
+  CharacterBWalkSheet1: new ImageSource(CharacterBWalkSheet1),
+  CharacterBWalkSheet2: new ImageSource(CharacterBWalkSheet2),
+  CharacterBWalkSheet3: new ImageSource(CharacterBWalkSheet3),
+  CharacterBWalkSheet4: new ImageSource(CharacterBWalkSheet4),
+  // BGmusic: new Sound(bgMusic),
+  // Battle: new Sound(battle),
+};
 // met deze for loop hoef je niet alles handmatig in de loader te zetten
 const resourceArray = [];
 for (const key in Resources) {
@@ -122,7 +126,5 @@ ResourceLoader.logo = logo;
 ResourceLoader.logoWidth = 478;
 ResourceLoader.logoHeight = 241;
 ResourceLoader.backgroundColor = new Color(145, 190, 97, 1);
-
-
 
 export { Resources, ResourceLoader };
--- src/js/scenes/game_lvl1.js
+++ src/js/scenes/game_lvl1.js
@@ -1,193 +1,266 @@
-import { Actor, Engine, Vector, Label, Font, Color, Random, Input, CollisionType, CollisionGroup, BoundingBox, EdgeCollider, Scene, Timer, randomInRange, Physics, CollisionGroupManager} from "excalibur"
-import { Resources, ResourceLoader } from '../resources.js'
-import { player } from '../player.js'
+import {
+  Actor,
+  Engine,
+  Vector,
+  Label,
+  Font,
+  Color,
+  Random,
+  Input,
+  CollisionType,
+  CollisionGroup,
+  BoundingBox,
+  EdgeCollider,
+  Scene,
+  Timer,
+  randomInRange,
+  Physics,
+  CollisionGroupManager,
+} from "excalibur";
+import { Resources, ResourceLoader } from "../resources.js";
+import { player } from "../player.js";
 import { IngameButton } from "../ingameButton.js";
 import { Wall } from "../wall.js";
-import { Box } from "../box.js"
-import {MovableObject} from "../movableObject.js";
-import {Secret_sign} from "../secret_sign.js";
-import {PigeonLabels} from "../pigeonLabels.js";
+import { Box } from "../box.js";
+import { MovableObject } from "../movableObject.js";
+import { Secret_sign } from "../secret_sign.js";
+import { PigeonLabels } from "../pigeonLabels.js";
+import musicFile from "../../sounds/BG_music.mp3";
 
 export class Level1 extends Scene {
-    game;
-    userInterface;
-    character;
-    breakable = CollisionType.Fixed;
-    player1ID;
-    player2ID;
-    remainingTime;
-    background;
-    firstSign;
-    constructor() {
+  bgMusic;
+  game;
+  userInterface;
+  character;
+  breakable = CollisionType.Fixed;
+  player1ID;
+  player2ID;
+  remainingTime;
+  background;
+  firstSign;
 
-        super({ width: 1280, height: 720 })
-        Physics.useRealisticPhysics();
+  constructor() {
+    super({ width: 1280, height: 720 });
+    Physics.useRealisticPhysics();
 
-        //#region buttons
-        this.button1 = new IngameButton(2500, -150, 1, this);
-        this.button2 = new IngameButton(1300, -400, 2, this);
-        this.button3 = new IngameButton(500, -200, 2, this);
-        this.button4 = new IngameButton(1680, 130, 3, this);
+    //#region buttons
+    this.button1 = new IngameButton(2500, -150, 1, this);
+    this.button2 = new IngameButton(1300, -400, 2, this);
+    this.button3 = new IngameButton(500, -200, 2, this);
+    this.button4 = new IngameButton(1680, 130, 3, this);
 
-        this.mazeButton = new IngameButton(750, 1150, 2, this);
-        this.mazeButton2 = new IngameButton(-50, 550, 1, this);
-        //#endregion buttons
+    this.mazeButton = new IngameButton(750, 1150, 2, this);
+    this.mazeButton2 = new IngameButton(-50, 550, 1, this);
+    //#endregion buttons
 
-        //#region walls
-        this.wall = new Wall(-1095, 165, 915, 165);
+    //#region walls
+    this.wall = new Wall(-1095, 165, 915, 165);
 
-       this.breakingWallImg = new MovableObject(1, Resources.Onewall.toSprite(), CollisionType.Fixed, 1005, -175);
-        this.breakingWallFence = new MovableObject(1, Resources.Fence.toSprite(), CollisionType.Fixed, 1850, 200);
+    this.breakingWallImg = new MovableObject(
+      1,
+      Resources.Onewall.toSprite(),
+      CollisionType.Fixed,
+      1005,
+      -175
+    );
+    this.breakingWallFence = new MovableObject(
+      1,
+      Resources.Fence.toSprite(),
+      CollisionType.Fixed,
+      1850,
+      200
+    );
 
-        this.breakingWallRock = new MovableObject(1, Resources.Moverock.toSprite(), CollisionType.Fixed, -250, 950);
-        //#endregion walls
+    this.breakingWallRock = new MovableObject(
+      1,
+      Resources.Moverock.toSprite(),
+      CollisionType.Fixed,
+      -250,
+      950
+    );
+    //#endregion walls
 
-        //#region boxes
-        this.boxes = new Box(250, 350, 1600, 200);
-        this.boxes2 = new Box(250, 1350, 1600, 200);
-        this.boxes3 = new Box(250, 750, 800, 200);
-        this.boxes4 = new Box(950, 950, 200, 600);
-        this.boxes5 = new Box(250, 1150, 800, 200);
-        this.boxes6 = new Box(-250, 550, 200, 200);
-        //#endregion boxes
+    //#region boxes
+    this.boxes = new Box(250, 350, 1600, 200);
+    this.boxes2 = new Box(250, 1350, 1600, 200);
+    this.boxes3 = new Box(250, 750, 800, 200);
+    this.boxes4 = new Box(950, 950, 200, 600);
+    this.boxes5 = new Box(250, 1150, 800, 200);
+    this.boxes6 = new Box(-250, 550, 200, 200);
+    //#endregion boxes
 
-        //#region objects
-        this.movableObject = new MovableObject(3, Resources.Smallstone.toSprite(), CollisionType.Active, -500, -200);
-        this.nextLevel = new MovableObject(1, Resources.Ending.toSprite(), CollisionType.Passive, -1050, 1050)
-        this.moveStone = new MovableObject(1, Resources.Stone.toSprite(), CollisionType.Active, 1000, 550);
-        //#endregion objects
-        this.firstPigeon = new PigeonLabels();
-        this.firstSign = new Secret_sign(this, -900, -400, this.firstPigeon.pigeonLabel1);
-        this.firstSign2 = new Secret_sign(this, 2000, -500, this.firstPigeon.pigeonLabel2);
-        this.firstSign3 = new Secret_sign(this, 2300, 1300, this.firstPigeon.pigeonLabel3);
+    //#region objects
+    this.movableObject = new MovableObject(
+      3,
+      Resources.Smallstone.toSprite(),
+      CollisionType.Active,
+      -500,
+      -200
+    );
+    this.nextLevel = new MovableObject(
+      1,
+      Resources.Ending.toSprite(),
+      CollisionType.Passive,
+      -1050,
+      1050
+    );
+    this.moveStone = new MovableObject(
+      1,
+      Resources.Stone.toSprite(),
+      CollisionType.Active,
+      1000,
+      550
+    );
+    //#endregion objects
+    this.firstPigeon = new PigeonLabels();
+    this.firstSign = new Secret_sign(
+      this,
+      -900,
+      -400,
+      this.firstPigeon.pigeonLabel1
+    );
+    this.firstSign2 = new Secret_sign(
+      this,
+      2000,
+      -500,
+      this.firstPigeon.pigeonLabel2
+    );
+    this.firstSign3 = new Secret_sign(
+      this,
+      2300,
+      1300,
+      this.firstPigeon.pigeonLabel3
+    );
+  }
 
+  onInitialize(engine) {
+    //gets engine to use for controllers, also adds background with right scale and pos
+    this.game = engine;
+    engine.input.gamepads.enabled = true;
 
+    let bg = new Actor();
+    bg.graphics.use(Resources.Bg.toSprite());
+    bg.scale = new Vector(26, 22);
+    bg.pos = new Vector(775, 480);
+    bg.z = 0;
+    this.add(bg);
 
-    }
+    //this is the playable background
+    this.background = new Actor();
+    this.background.graphics.use(Resources.realLevel.toSprite());
+    this.background.scale = new Vector(2.6, 2.2);
+    this.background.pos = new Vector(775, 480);
+    this.add(this.background);
 
-    onInitialize(engine) {
-        //gets engine to use for controllers, also adds background with right scale and pos
-        this.game = engine
-        engine.input.gamepads.enabled = true;
+    //#region music
+    this.bgMusic = new Audio(musicFile);
+    this.bgMusic.loop = true;
+    this.bgMusic.play();
+    //#endregion music
 
+    console.log("adding the bg music via oninitialise");
+  }
 
-        let bg = new Actor();
-        bg.graphics.use(Resources.Bg.toSprite());
-        bg.scale = new Vector(26, 22);
-        bg.pos = new Vector(775, 480);
-        bg.z = 0;
-        this.add(bg);
+  onActivate(ctx) {
+    //adds players to scene and gives them the right id and location
+    console.log("Level1 has started via onactivate");
+    console.log(ctx.previousScene);
 
-        //this is the playable background
-        this.background = new Actor();
-        this.background.graphics.use(Resources.realLevel.toSprite());
-        this.background.scale = new Vector(2.6, 2.2);
-        this.background.pos = new Vector(775, 480);
-        this.add(this.background);
-    }
+    // this.bgMusic.play();
+    console.log(this.bgMusic);
+    this.player1ID = ctx.previousScene.player1ID;
+    this.player2ID = ctx.previousScene.player2ID;
+    this.player = new player(this, 1, -700, 75, this.player1ID, 0, 1);
+    this.player2 = new player(this, 2, -700, -225, this.player2ID, 0, 0);
+    this.startGame();
+  }
 
-    onActivate(ctx) {
-        //adds players to scene and gives them the right id and location
-        console.log("Level1 has started");
-        console.log(ctx.previousScene)
-        this.player1ID = ctx.previousScene.player1ID
-        this.player2ID = ctx.previousScene.player2ID
-        this.player = new player(this, 1, -700, 75, this.player1ID, 0, 1,);
-        this.player2 = new player(this,2, -700, -225, this.player2ID, 0, 0);
-        this.startGame()
-    }
+  startGame() {
+    this.game.currentScene.camera.strategy.lockToActor(this.player);
+    this.game.currentScene.camera.zoom = 0.9;
+    this.add(this.player);
+    this.add(this.player2);
 
-    startGame() {
-        this.game.currentScene.camera.strategy.lockToActor(this.player)
-        this.game.currentScene.camera.zoom = 0.9
-        this.add(this.player);
-        this.add(this.player2)
+    //#region addedButtons
+    this.add(this.button1);
+    this.add(this.button2);
+    this.add(this.button3);
+    this.add(this.mazeButton);
+    this.add(this.mazeButton2);
+    //#endregion addedButtons
 
-        //#region addedButtons
-        this.add(this.button1);
-        this.add(this.button2);
-        this.add(this.button3);
-        this.add(this.mazeButton);
-        this.add(this.mazeButton2);
-        //#endregion addedButtons
+    //#region addedWalls
+    this.add(this.wall);
+    //#endregion addedWalls
 
-        //#region addedWalls
-        this.add(this.wall);
-        //#endregion addedWalls
+    //#region addedBoxes
+    //all boxes used for the maze
+    this.add(this.boxes);
+    this.add(this.boxes2);
+    this.add(this.boxes3);
+    this.add(this.boxes4);
+    this.add(this.boxes5);
+    this.add(this.boxes6);
+    //#endregion addedBoxes
 
-        //#region addedBoxes
-        //all boxes used for the maze
-        this.add(this.boxes);
-        this.add(this.boxes2);
-        this.add(this.boxes3);
-        this.add(this.boxes4);
-        this.add(this.boxes5);
-        this.add(this.boxes6);
-        //#endregion addedBoxes
+    this.add(this.movableObject);
+    this.add(this.moveStone);
 
-        this.add(this.movableObject);
-        this.add(this.moveStone);
+    this.add(this.nextLevel);
 
-        this.add(this.nextLevel)
-
-        this.add(this.breakingWallImg);
-        this.add(this.breakingWallFence);
-        this.add(this.breakingWallRock);
-        this.add(this.firstSign);
-        this.add(this.firstSign2);
-        this.add(this.firstSign3);
-
-    }
-    /*
+    this.add(this.breakingWallImg);
+    this.add(this.breakingWallFence);
+    this.add(this.breakingWallRock);
+    this.add(this.firstSign);
+    this.add(this.firstSign2);
+    this.add(this.firstSign3);
+  }
+  /*
     loadEverything() {
       this.add(this.player)
 
     }
     */
-    onPreUpdate(game){
-        //this is puzzle one, both press the button and the door opens
-        //these are all if statements used to check for buttons etc.
-        if (this.button1.openDoor && this.button2.openDoor) {
-            Resources.Opendoor.play();
-            this.breakingWallFence.kill();
-            this.button1.openDoor = false;
-            this.button2.openDoor = false;
-        }
-
-        if (this.mazeButton.openDoor && this.mazeButton2.openDoor) {
-            this.breakingWallRock.kill();
-            this.mazeButton.openDoor = false;
-            this.mazeButton2.openDoor = false;
-        }
-
-
-        //this is puzzle two, if the button gets pressed
-        if (this.button3.openDoor) {
-            this.breakingWallImg.kill();
-            this.button3.opendoor = false;
-        } else {
-            this.add(this.breakingWallImg);
-            this.button3.opendoor = false;
-        }
-
-        //last button checker, only works if movable object is used
-        if (this.button4.openDoor) {
-            Resources.Opendoor.play();
-            this.button4.opendoor = false;
-        }
-
-        if (this.nextLevel.isColliding) {
-            this.game.startLevel2();
-            this.nextLevel.isColliding = false;
-        }
-
-
-        //also adds keyboard and controller to use ingame
-        let kb = this.game.input.keyboard
-        let controller = this.game.input.gamepads
-        if (kb.isHeld(Input.Keys.Enter)) {
-            
-
-        }
+  onPreUpdate(game) {
+    //this is puzzle one, both press the button and the door opens
+    //these are all if statements used to check for buttons etc.
+    if (this.button1.openDoor && this.button2.openDoor) {
+      Resources.Opendoor.play();
+      this.breakingWallFence.kill();
+      this.button1.openDoor = false;
+      this.button2.openDoor = false;
     }
+
+    if (this.mazeButton.openDoor && this.mazeButton2.openDoor) {
+      this.breakingWallRock.kill();
+      this.mazeButton.openDoor = false;
+      this.mazeButton2.openDoor = false;
+    }
+
+    //this is puzzle two, if the button gets pressed
+    if (this.button3.openDoor) {
+      this.breakingWallImg.kill();
+      this.button3.opendoor = false;
+    } else {
+      this.add(this.breakingWallImg);
+      this.button3.opendoor = false;
+    }
+
+    //last button checker, only works if movable object is used
+    if (this.button4.openDoor) {
+      Resources.Opendoor.play();
+      this.button4.opendoor = false;
+    }
+
+    if (this.nextLevel.isColliding) {
+      this.game.startLevel2();
+      this.nextLevel.isColliding = false;
+    }
+
+    //also adds keyboard and controller to use ingame
+    let kb = this.game.input.keyboard;
+    let controller = this.game.input.gamepads;
+    if (kb.isHeld(Input.Keys.Enter)) {
+    }
+  }
 }
